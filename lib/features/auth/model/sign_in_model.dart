1779fec59bcc02b2364ff5531c296775fef996e4
khaled <khaldmysrya@gmail.com>
2025-08-03 18:17:53 +0300

    Comment out SignInModel class and its methods for future reference

--- lib/features/auth/model/sign_in_model.dart
+++ lib/features/auth/model/sign_in_model.dart
@@ -1,14 +1,14 @@
-import 'package:cash_flow/core/database/api/endpoint.dart';
+// import 'package:cash_flow/core/database/api/endpoint.dart';
 
-class SignInModel {
-  final String message;
-  final String token;
-  factory SignInModel.fromMap(Map<String, dynamic> map) {
-    return SignInModel(
-      message: map[ApiKey.message] as String,
-      token: map[ApiKey.token] as String,
-    );
-  }
+// class SignInModel {
+//   final String message;
+//   final String token;
+//   factory SignInModel.fromMap(Map<String, dynamic> map) {
+//     return SignInModel(
+//       message: map[ApiKey.message] as String,
+//       token: map[ApiKey.token] as String,
+//     );
+//   }
 
-  SignInModel({required this.message, required this.token});
-}
+//   SignInModel({required this.message, required this.token});
+// }
