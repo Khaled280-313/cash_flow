b6ddd980a9de912d7148bd8d9c453b63d4721d81
khaled <khaldmysrya@gmail.com>
2025-08-11 19:22:33 +0300

    Usecases Get User

---
+++ lib/features/setting/domain/usecases/get_user.dart
@@ -0,0 +1,14 @@
+import 'package:cash_flow/features/setting/domain/repo/user_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/user_entities.dart';
+
+class GetUser {
+  final UserRepo user;
+
+  GetUser({required this.user});
+  Future<Either<Failure, UserEntities>> call() {
+    return user.getUser();
+  }
+}
