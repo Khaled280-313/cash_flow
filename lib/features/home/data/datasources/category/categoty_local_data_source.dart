2758cf0ae21a0f4e8d1067090d420c045c0227f8
khaled <khaldmysrya@gmail.com>
2025-08-13 20:03:10 +0300

    CategoryLocalDataSource

---
+++ lib/features/home/data/datasources/category/categoty_local_data_source.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:cash_flow/core/database/cache/cache_helper.dart';
+import 'package:cash_flow/core/services/servic_locator.dart';
+
+import '../../../../../core/errors/exception.dart';
+import '../../model/category/category_model.dart';
+
+class CategoryLocalDataSource {
+  Future categoryCache(List<CategoryModel>? categories) async {
+    if (categories == null) {
+      throw CacheException(errorMessage: "No user to cache");
+    }
+    final jsonString = jsonEncode(categories.map((c) => c.toJson()).toList());
+    getIt<CacheHelper>().saveData(key: "cached_categories", value: jsonString);
+  }
+
+  Future<List<CategoryModel>> getCachedCategories() async {
+    final jsonString =
+        await getIt<CacheHelper>().getData(key: "cached_categories");
+    if (jsonString == null || (jsonString is String && jsonString.isEmpty)) {
+      throw CacheException(errorMessage: "No cached categories found");
+    }
+    final List<dynamic> jsonList = jsonDecode(jsonString);
+
+    return jsonList.map((json) => CategoryModel.fromJson(json)).toList();
+  }
+}
