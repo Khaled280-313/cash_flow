10951146d3fccb52ea8564d4220870ffad93c86b
khaled <khaldmysrya@gmail.com>
2025-08-13 19:28:06 +0300

    create Category Repo

---
+++ lib/features/home/model/repo/category/category_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../../core/errors/failure.dart';
+import '../../entities/catecory/category_entities.dart';
+
+abstract class CategoryRepo {
+  Future<Either<Failure, List<CategoryEntities>>> getCategories();
+  Future<Either<Failure, void>> addCategory(CategoryEntities category);
+  Future<Either<Failure, void>> updateCategory(CategoryEntities category);
+  Future<Either<Failure, void>> deleteCategory(String id);
+}
