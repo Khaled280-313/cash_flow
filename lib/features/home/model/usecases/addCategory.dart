648b4f69af7d37b02b4a6fdf597629693e2b368f
khaled <khaldmysrya@gmail.com>
2025-08-13 19:28:40 +0300

    create use case AddCategory

---
+++ lib/features/home/model/usecases/addCategory.dart
@@ -0,0 +1,15 @@
+import 'package:cash_flow/core/errors/failure.dart' ;
+import 'package:cash_flow/features/home/model/repo/category/category_repo.dart' show CategoryRepo;
+import 'package:dartz/dartz.dart';
+
+import '../entities/catecory/category_entities.dart';
+
+class AddCategory {
+  final CategoryRepo categoryRepo;
+
+  AddCategory({required this.categoryRepo});
+
+  Future<Either<Failure, void>> call(CategoryEntities category) {
+    return categoryRepo.addCategory(category);
+  }
+}
