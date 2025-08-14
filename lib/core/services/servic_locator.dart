6b71e36b99c1ef71e163b8ecce3e9d4cd2d7dc3c
khaled <khaldmysrya@gmail.com>
2025-08-14 19:25:12 +0300

    fix code

--- lib/core/services/servic_locator.dart
+++ lib/core/services/servic_locator.dart
@@ -1,28 +1,53 @@
+import 'package:cash_flow/core/database/api/endpoint.dart';
 import 'package:cash_flow/core/database/cache/cache_helper.dart';
+
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:dio/dio.dart';
 
 import 'package:get_it/get_it.dart';
 
+import '../../features/home/data/datasources/category/category_remot_data_source.dart';
+import '../../features/home/data/datasources/category/categoty_local_data_source.dart';
 import '../../features/transactions/presentation/cubit/transactions_cubit.dart';
 import '../connection/network_info.dart';
 import '../database/api/dio_consumer.dart';
 
 final getIt = GetIt.instance;
-void setupServiceLocator() {
-  // Register your services and repositories here
-
+void setupServiceLocator() async {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
+
   getIt.registerLazySingleton(() => DioConsumer(dio: Dio()));
 
-  getIt.registerSingleton<TransactionsCubit>(TransactionsCubit());
+  final dio = Dio(BaseOptions(baseUrl: Endpoint.baseUrl));
+  getIt.registerLazySingleton<Dio>(() => dio);
+
+  getIt
+      .registerLazySingleton<DioConsumer>(() => DioConsumer(dio: getIt<Dio>()));
+
   getIt.registerLazySingleton<DataConnectionChecker>(
       () => DataConnectionChecker());
 
   getIt.registerLazySingleton<NetworkInfo>(() =>
       NetworkInfoImpl(dataConnectionChecker: getIt<DataConnectionChecker>()));
 
-  // getIt.registerSingleton<SizeConfig>(SizeConfig());
-  // getIt.registerLazySingleton(() => DatabaseHelper());
-  // getIt.registerFactory(() => UserRepository(dbHelper: getIt<DatabaseHelper>()));
+  getIt.registerLazySingleton<NetworkInfo>(() =>
+      NetworkInfoImpl(dataConnectionChecker: getIt<DataConnectionChecker>()));
+
+  getIt.registerLazySingleton<CategoryLocalDataSource>(
+      () => CategoryLocalDataSource());
+
+  getIt.registerLazySingleton<CategoryRemoteDataSource>(
+      () => CategoryRemoteDataSource(api: getIt<DioConsumer>()));
+
+  // --- Repositories / Usecases (example) ---
+  // getIt.registerLazySingleton<CategoryRepository>(() =>
+  //   CategoryRepositoryImpl(remoteDataSource: getIt(), localDataSource: getIt(), networkInfo: getIt()));
+  // getIt.registerLazySingleton(() => GetCategories(getIt()));
+
+  // --- Cubits / Blocs ---
+  // For Cubits/Blocs prefer registerFactory (new instance per injection)
+  getIt.registerFactory<TransactionsCubit>(() => TransactionsCubit());
+  // e.g. getIt.registerFactory(() => CategoriesCubit(getCategories: getIt()));
+
+  // done
 }
