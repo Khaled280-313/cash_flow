8f832fc88c3a96e43736da3d55a3be7f121590cc
khaled <khaldmysrya@gmail.com>
2025-08-17 19:26:46 +0300

    feat: implement budget addition functionality with state management and UI updates

--- lib/features/budgets/presentation/pages/add_budgets.dart
+++ lib/features/budgets/presentation/pages/add_budgets.dart
@@ -1,19 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/utils/app_color.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../../../home/presentation/cubit/home_cubit.dart';
+import '../../../transactions/presentation/widgets/custom_cntain_togle.dart';
 
 class AddBudgets extends StatelessWidget {
   const AddBudgets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Budgets'),
+    HomeCubit home = BlocProvider.of<HomeCubit>(context);
+    return BlocConsumer<HomeCubit, HomeState>(
+      listener: (context, state) {
+        if (state is CategoryAdded) {
+          home.getCategories();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.category.message)),
+          );
+        } else if (state is CategoryError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.message)),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+            appBar: AppBar(
+              title: const Text('Add Budgets'),
+            ),
+            body: Form(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CustomTogleButton(
+                      isSelected: [
+                        home.type == 'INCOME',
+                        home.type == 'EXPENSE'
+                      ],
+                      onPressed: (index) {
+                        home.type = index == 0 ? 'INCOME' : 'EXPENSE';
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        home.name = value;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
+                  state is CategoryLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : ElevatedButton(
+                          onPressed: () {
+                            home.addCategory();
+                          },
+                          child: const Text('Submit'),
+                        ),
+                ],
+              ),
+            ));
+      },
+    );
+  }
+}
+
+class CustomTogleButton extends StatefulWidget {
+  const CustomTogleButton({
+    super.key,
+    required this.isSelected,
+    this.onPressed,
+  });
+  final List<bool> isSelected;
+  final void Function(int)? onPressed;
+
+  @override
+  State<CustomTogleButton> createState() => _CustomTogleButtonState();
+}
+
+class _CustomTogleButtonState extends State<CustomTogleButton> {
+  bool isSelected = true;
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      borderColor: AppColor.white,
+      borderRadius: BorderRadius.circular(30),
+      selectedBorderColor: AppColor.white,
+      splashColor: AppColor.white,
+      constraints: const BoxConstraints(
+        minWidth: 167,
+        minHeight: 48,
       ),
-      body: Center(
-        child: Text(
-          'Add your budget details here',
+      fillColor: AppColor.white,
+      borderWidth: 3,
+      onPressed: ((index) {
+        setState(() {
+          isSelected = !isSelected;
+          widget.onPressed?.call(index);
+        });
+      }),
+      isSelected: widget.isSelected,
+      children: [
+        CustomCntainTogle(
+          text: AppStrings.income,
+          isSelected: isSelected,
         ),
-      ),
+        CustomCntainTogle(
+          text: AppStrings.expenses,
+          isSelected: !isSelected,
+        ),
+      ],
     );
   }
 }
