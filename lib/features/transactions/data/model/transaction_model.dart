4bcfd17c5bf25179f758d99c20587467ad1fbe25
khaled <khaldmysrya@gmail.com>
2025-08-18 15:10:14 +0300

    refactor: simplify TransactionModel by removing unused id property and streamlining JSON serialization

--- lib/features/transactions/data/model/transaction_model.dart
+++ lib/features/transactions/data/model/transaction_model.dart
@@ -1,39 +1,48 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
 import '../../domain/entities/transaction_entities.dart';
 
 class TransactionModel extends TransactionEntities {
-  final String id;
-
-  final String currency;
-  TransactionModel({
-    required this.id,
-    required this.currency,
-    required super.description,
-    required super.amount,
-    required super.date,
-    // required super.time,
-  });
-
+  TransactionModel(
+      {required super.amount,
+      required super.date,
+      required super.description,
+      required super.currency,
+      required super.category});
   factory TransactionModel.fromJson(Map<String, dynamic> json) {
     return TransactionModel(
-      id: json['id'] as String,
-      currency: json['currency'] as String,
-      description: json['description'] as String,
       amount: (json['amount'] as num).toDouble(),
       date: DateTime.parse(json['date'] as String),
-      // time: DateTime.parse(json['time'] as String),
+      description: json['description'] as String,
+      currency: json['currency'] as String,
+      category: json['category'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'currency': currency,
-      'description': description,
       'amount': amount,
       'date': date.toIso8601String(),
-      // 'time': time.toString(),
+      'description': description,
+      'currency': currency,
+      'category': category,
     };
   }
+
+  TransactionEntities toEntity() {
+    return TransactionEntities(
+      amount: amount,
+      date: date,
+      description: description,
+      currency: currency,
+      category: category,
+    );
+  }
+
+  factory TransactionModel.fromEntity(TransactionEntities entity) =>
+      TransactionModel(
+        amount: entity.amount,
+        date: entity.date,
+        description: entity.description,
+        currency: entity.currency,
+        category: entity.category,
+      );
 }
