0a5693ccf24ec05a9915d2f3f41a432d455c4764
khaled <khaldmysrya@gmail.com>
2025-08-18 13:08:01 +0300

    feat: add formattedBirthDate getter to TransactionsCubit for date formatting

--- lib/features/transactions/presentation/cubit/transactions_cubit.dart
+++ lib/features/transactions/presentation/cubit/transactions_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 part 'transactions_state.dart';
 
@@ -9,6 +10,8 @@ class TransactionsCubit extends Cubit<TransactionsState> {
   // TextEditingController categoryController = TextEditingController();
   late num amount;
   DateTime selectedDate = DateTime.now();
+  String get formattedBirthDate =>
+      DateFormat('yyyy-MM-dd').format(selectedDate);
   late String description;
   late String currency;
   late String selectedCategory;
